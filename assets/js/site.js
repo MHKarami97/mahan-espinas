4abd925fe510f346b05b6f871c67465152b0eda7
mhkarami97 <mhkarami97@gmail.com>
2026-07-15 10:49:33 +0330

    change

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,11 @@
-(function(){document.documentElement.setAttribute('data-theme','light');})();
+(function(){
+  document.documentElement.setAttribute('data-theme','light');
+  var btn = document.getElementById('navBurgerBtn');
+  var header = document.querySelector('.site-header');
+  if(btn && header){
+    btn.addEventListener('click', function(){
+      var open = header.classList.toggle('menu-open');
+      btn.setAttribute('aria-expanded', open ? 'true' : 'false');
+    });
+  }
+})();
